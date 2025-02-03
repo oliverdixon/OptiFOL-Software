1d175c6b1fb991621318d2ce77a6ef33f1a2733f
Oliver Dixon <od641@york.ac.uk>
2025-02-03 22:00:45 +0000

    Added notify-based directory listing

--- Source/GUI/MainWindow.cpp
+++ Source/GUI/MainWindow.cpp
@@ -4,15 +4,51 @@
 
 #include "MainWindow.hpp"
 
-#include <iostream>
+#include "GTKHelpers.hpp"
 
 namespace optifol
 {
 
 MainWindow::MainWindow()
 {
+    builder = Gtk::Builder::create_from_resource("/uk/ac/york/www_users/od641/optifol/UI/MainWindow.ui");
+    scrolled_window = GTKHelpers::get_widget<Gtk::ScrolledWindow>("Main Window", builder, "scrolled_window");
+    directory_view = GTKHelpers::get_widget<Gtk::ListView>("Main Window", builder, "directory_view");
+
     set_title("OptiFOL");
     set_default_size(600, 400);
+    set_child(*scrolled_window);
+
+    const auto selection_model = Gtk::SingleSelection::create(directory_model);
+    selection_model->set_autoselect(false);
+    selection_model->set_can_unselect(true);
+    directory_view->set_model(selection_model);
+
+    const auto factory = Gtk::SignalListItemFactory::create();
+    factory->signal_setup().connect(&MainWindow::on_setup_label);
+    factory->signal_bind().connect(sigc::mem_fun(*this, &MainWindow::on_bind_name));
+    directory_view->set_factory(factory);
+}
+
+void MainWindow::on_setup_label(const Glib::RefPtr<Gtk::ListItem> &item)
+{
+    item->set_child(*Gtk::make_managed<Gtk::Label>("", Gtk::Align::START));
+}
+
+void MainWindow::on_bind_name(const Glib::RefPtr<Gtk::ListItem> &item) const
+{
+    const auto pos = item->get_position();
+
+    if (pos != GTK_INVALID_LIST_POSITION) {
+        const auto label = dynamic_cast<Gtk::Label*>(item->get_child());
+        if (label != nullptr) {
+            const auto info = directory_model->get_typed_object<Gio::FileInfo>(pos);
+            if (info != nullptr) {
+                const auto& prefix = directory_model->get_file()->get_path();
+                label->set_text(prefix + '/' + info->get_name());
+            }
+        }
+    }
 }
 
 }
--- Source/GUI/MainWindow.hpp
+++ Source/GUI/MainWindow.hpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) All Rights Reserved
+ * 2024--2025 Oliver Dixon <od641@york.ac.uk>
+ */
+
 //
 // Created by owd on 18/01/25.
 //
@@ -15,6 +20,19 @@ class MainWindow :
 {
 public:
     MainWindow();
+
+private:
+    static void on_setup_label(const Glib::RefPtr<Gtk::ListItem> &item);
+
+    void on_bind_name(const Glib::RefPtr<Gtk::ListItem>& item) const;
+
+    Gtk::ScrolledWindow * scrolled_window;
+    Gtk::ListView * directory_view;
+
+    Glib::RefPtr<Gtk::DirectoryList> directory_model =
+        Gtk::DirectoryList::create("standard::name", Gio::File::create_for_path("/home/owd/"));
+
+    Glib::RefPtr<Gtk::Builder> builder;
 };
 
 }
