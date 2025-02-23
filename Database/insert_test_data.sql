5e017b37f3e3c3df5676da7ee8ba71f0b3edf325
Oliver Dixon <od641@york.ac.uk>
2025-02-23 13:52:16 +0000

    Superficial changes to DB structure

---
+++ Database/insert_test_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO project (name)
+VALUES ('Astute'),
+       ('Vanguard'),
+       ('Dreadnought');
+
+INSERT INTO subsystem (project_id, name)
+VALUES (1, 'Sonar'),
+       (1, 'Comms'),
+       (1, 'Propulsion'),
+       (2, 'Sonar'),
+       (2, 'Comms'),
+       (2, 'Propulsion'),
+       (3, 'Sonar'),
+       (3, 'Comms'),
+       (3, 'Propulsion');
